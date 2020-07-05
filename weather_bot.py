fcaee1f18ec44aed7a65d54b71f6a599aed7b14b
paul.minogue <paul.minogue@edgetier.com>
2020-07-05 22:52:52 +0100

    Added Aisling to the recipient list

--- weather_bot.py
+++ weather_bot.py
@@ -9,22 +9,25 @@ url = f"https://api.openweathermap.org/data/2.5/forecast?id={os.environ['OPEN_WE
       f"APPID={os.environ['OPEN_WEATHER_TOKEN']}&mode=json&units=metric"
 
 data = requests.get(url).json()
+recipients = [{'name': 'Paul', 'chat_id': os.environ['PAUL_TELEGRAM_CHAT_ID']},
+              {'name': 'Aisling', 'chat_id': os.environ['AISLING_TELEGRAM_CHAT_ID']}]
 
-# construct the message we're going to send by examining the first few entries in the JSON forecast
-weather_message = "Morning Paul,\n\nHere is today's forecast:\n"
+for recipient in recipients:
+    # construct the message we're going to send by examining the first few entries in the JSON forecast
+    weather_message = f"Morning {recipient['name']},\n\nHere is today's forecast:\n"
 
-for forecast in data['list'][0:5]:
-    time_as_dt = datetime.strptime(forecast['dt_txt'], '%Y-%m-%d %H:%M:%S')
+    for forecast in data['list'][0:5]:
+        time_as_dt = datetime.strptime(forecast['dt_txt'], '%Y-%m-%d %H:%M:%S')
 
-    weather_message += (f"\nTime: {time_as_dt.strftime('%H:%M:%S')}\n"
-                        f"Description: {forecast['weather'][0]['description'].title()}\n"
-                        f"Teamperature: {int(forecast['main']['temp'])}\n"
-                        f"Feels like: {int(forecast['main']['feels_like'])}\n"
-                        f"Wind speed: {int(forecast['wind']['speed'] * 3.6)}Kph\n")
+        weather_message += (f"\nTime: {time_as_dt.strftime('%H:%M:%S')}\n"
+                            f"Description: {forecast['weather'][0]['description'].title()}\n"
+                            f"Teamperature: {int(forecast['main']['temp'])}\n"
+                            f"Feels like: {int(forecast['main']['feels_like'])}\n"
+                            f"Wind speed: {int(forecast['wind']['speed'] * 3.6)}Kph\n")
 
-# send message using the telegram API
-send_message_url = ('https://api.telegram.org/bot' + os.environ['TELEGRAM_BOT_TOKEN'] +
-    '/sendMessage?chat_id=' + os.environ['TELEGRAM_CHAT_ID'] +
-    '&text=' + weather_message.replace(' ', '+').replace('\n', '%0A')) # <- need to replace special characters
+    # send message using the telegram API
+    send_message_url = ('https://api.telegram.org/bot' + os.environ['TELEGRAM_BOT_TOKEN'] +
+        '/sendMessage?chat_id=' + recipient['chat_id'] +
+        '&text=' + weather_message.replace(' ', '+').replace('\n', '%0A')) # <- need to replace special characters
 
-requests.get(send_message_url)
+    requests.get(send_message_url)
